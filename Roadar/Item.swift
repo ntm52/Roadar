093cdd8dcde9f44affc2d40b220ec1f915127442
Nathan Mayo <n8mayo1@gmail.com>
2026-09-03 20:04:36 -0400

    Initial Commit

---
+++ Roadar/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Roadar
+//
+//  Created by Nathan Mayo on 9/3/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ RoadarTests/RoadarTests.swift
@@ -0,0 +1,19 @@
+//
+//  RoadarTests.swift
+//  RoadarTests
+//
+//  Created by Nathan Mayo on 9/3/26.
+//
+
+import Testing
+@testable import Roadar
+
+struct RoadarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
